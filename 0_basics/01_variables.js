711aaa19d05e06289edffb8c92fb7d94ebf21b06
Chetan Singh <chetansingh@Chetans-MacBook-Air.local>
2024-07-28 11:41:56 +0530

    first commit

---
+++ 0_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 8749385;
+let email = "chetan@gmail.com";
+var password = 948567;
+city = "palampur";
+let state;
+
+email = "beastboy122@gmial.com";
+password = 12321;
+city = "baddi";
+
+console.table([accountId, email, password, city, state]);
---
+++ 0_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //Treats all the code as newer version of js
+console.log(3 + 3);
+
+let name = "chetan";
+let age = 22;
+let male = true;
+
+// number
+// bigint
+// string
+// boolean
+// null => standalone value (is a repersentation of empty value)
+// undefined => declares that a variable is created but is not defined as of now
+// symbol => unique
+
+// object
+
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
