7a4fc4f619392d5b815166a46726f940b13eebef
Chetan Singh <chetansingh@Chetans-MacBook-Air.local>
2024-08-01 11:47:35 +0530

    numbers

--- 0_basics/07_string.js
+++ 0_basics/07_string.js
@@ -2,3 +2,5 @@ const name = "Chetan";
 const repo = 10;
 
 console.log(`My name is ${name} and my Repo count is ${repo}`);
+
+//https://youtu.be/fozwNnFunlo?si=DBfXd45rv7JxFqug    watch for revision of strings in js
---
+++ 0_basics/08_Number_Math.js
@@ -0,0 +1,24 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+// https://youtu.be/_KqpeDc47Ro?si=Dbzf6RJtkLUvxIfC  for revision
+
+// ÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷ MATHS ÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(10.21));
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
