dc90ba58bc66563eaa0decef652ec459ec09ecb9
SinghCtn <chetansingh2k1@gmail.com>
2024-08-15 00:02:42 +0530

    static

--- 10_classes_oops/07_staticProp.js
+++ 10_classes_oops/07_staticProp.js
@@ -1,1 +1,27 @@
-// this is static props
+class User {
+  constructor(username) {
+    this.username = username;
+  }
+
+  logMe() {
+    console.log(`Username is ${this.username}`);
+  }
+
+  static createId() {
+    return `123${this.username}`;
+  }
+}
+
+const chetan = new User("chetan");
+// console.log(chetan.createId());
+
+class Teacher extends User {
+  constructor(email, usename) {
+    super(usename);
+    this.email = email;
+  }
+}
+
+const iphone = new Teacher("iphone", "iphone@apple.com");
+iphone.logMe();
+console.log();
